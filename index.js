87b475f2c97df8c81831a2a7a73879e0967a2eba
Pumlani <lani.buhle@gmail.com>
2018-08-10 15:22:16 +0200

    setting bills with no color change

--- index.js
+++ index.js
@@ -1,29 +1,63 @@
 let express = require('express');
-let app = express();
-
+const SettingsBill = require('./SettingsBill');
+const bodyParser = require('body-parser');
 const exphbs = require('express-handlebars');
 
+let app = express();
+const settingsBill = SettingsBill();
+
+
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
+// parse application/json
+app.use(bodyParser.json())
+
+//configure handlebars
 app.engine('handlebars', exphbs({
     defaultLayout: 'main'
 }));
 app.set('view engine', 'handlebars');
 
 app.use(express.static('public'));
+//creating routs
+app.get('/', function (req, res) {
+    res.render('home', {
+        settings: settingsBill.getSettings(),
+        totals: settingsBill.totals()
+    });
+});
+app.post('/settings', function (req, res) {
+    settingsBill.setSettings({
+        callCost: req.body.callCost,
+        smsCost: req.body.smsCost,
+        warningLevel: req.body.warningLevel,
+        criticalLevel: req.body.criticalLevel
 
-app.get('/', function (req, res) {
-    res.render('home');
+    })
+    console.log();
+
+    res.redirect('/');
 });
-app.get('/', function (req, res) {
-    res.render('home');
-});
-app.get('/', function (req, res) {
-    res.render('home');
+app.post('/action', function (req, res) {
+
+    //capture button type to add
+    settingsBill.recordAction(req.body.billItemType)
+
+    res.redirect('/');
 });
 app.get('/actions', function (req, res) {
-    res.render('home');
+    res.render("actions", {
+        actions: settingsBill.actions()
+    });
 });
-app.get('/actions/:type', function (req, res) {
-    res.render('home');
+app.get('/actions/:billItemType', function (req, res) {
+    const billItemType = req.params.billItemType;
+    res.render("actions", {
+        actions: settingsBill.actionsFor(billItemType)
+    });
 });
 
 let PORT = process.env.PORT || 3007;
